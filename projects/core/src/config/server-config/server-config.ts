94362d4ecaa934d2514a3ec3323d769ee9bb449c
Krzysztof Platis <platonn.git@gmail.com>
2018-10-25 16:42:51 +0200

    feature/SPA-1403-demo: add support only for arrays of paths in config

--- projects/core/src/config/server-config/server-config.ts
+++ projects/core/src/config/server-config/server-config.ts
@@ -9,29 +9,29 @@ export abstract class ServerConfig {
   // TODO consider if all those default routes should be configured in separate modules
   // But do we need them also here in case they are not defined in separate modules then?
   routePaths?: {
-    homepage?: string | string[];
-    cart?: string | string[];
-    search?: string | string[];
-    login?: string | string[];
-    register?: string | string[];
-    resetNewPassword?: string | string[];
-    resetPassword?: string | string[];
-    checkout?: string | string[];
-    orderConfirmation?: string | string[];
-    product?: string | string[];
-    category?: string | string[];
-    storeFinder?: string | string[];
+    homepage?: string[];
+    cart?: string[];
+    search?: string[];
+    login?: string[];
+    register?: string[];
+    resetNewPassword?: string[];
+    resetPassword?: string[];
+    checkout?: string[];
+    orderConfirmation?: string[];
+    product?: string[];
+    category?: string[];
+    storeFinder?: string[];
 
     // myAccount pages
     // TODO: consider if should be prefixed with myAccount_ or not
-    myAccount_orders?: string | string[];
-    myAccount_orderDetails?: string | string[];
+    myAccount_orders?: string[];
+    myAccount_orderDetails?: string[];
 
-    contact?: string | string[];
-    help?: string | string[];
-    sale?: string | string[];
+    contact?: string[];
+    help?: string[];
+    sale?: string[];
 
-    pageNotFound?: string | string[];
+    pageNotFound?: string[];
   };
 }
 
@@ -41,31 +41,29 @@ export const defaultServerConfig: ServerConfig = {
   },
 
   routePaths: {
-    homepage: '',
-    cart: 'cart',
-    search: 'search/:query',
-    login: 'login',
-    register: 'register',
-    resetNewPassword: 'reset-new-password/:token',
-    resetPassword: 'reset-password',
-    checkout: 'checkout',
-    orderConfirmation: 'order-confirmation',
-    product: 'product/:productCode',
-    category: 'category/:categoryCode',
-    // spike todo: add support for arrays:
-    // [
-    //   'category/:categoryCode',
-    //   'category/:categoryCode/:title',
-    //   'Brands/:brandName/c/:brandCode'
-    // ],
-    storeFinder: 'store-finder',
+    homepage: [''],
+    cart: ['cart'],
+    search: ['search/:query'],
+    login: ['login'],
+    register: ['register'],
+    resetNewPassword: ['reset-new-password/:token'],
+    resetPassword: ['reset-password'],
+    checkout: ['checkout'],
+    orderConfirmation: ['order-confirmation'],
+    product: ['product/:productCode'],
+    category: [
+      'category/:categoryCode',
+      'category/:categoryCode/:title',
+      'Brands/:brandName/c/:brandCode'
+    ],
+    storeFinder: ['store-finder'],
 
-    myAccount_orders: 'my-account/orders',
-    myAccount_orderDetails: 'my-account/orders/:orderCode',
+    myAccount_orders: ['my-account/orders'],
+    myAccount_orderDetails: ['my-account/orders/:orderCode'],
 
-    contact: 'contact',
-    help: 'help',
-    sale: 'sale',
-    pageNotFound: '**'
+    contact: ['contact'],
+    help: ['help'],
+    sale: ['sale'],
+    pageNotFound: ['**']
   }
 };
--- projects/storefrontlib/src/lib/ui/layout/main/storefront.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/main/storefront.component.ts
@@ -13,53 +13,55 @@ export class StorefrontComponent implements OnInit {
   constructor(private config: AuthModuleConfig, private router: Router) {}
 
   // convention: '__cx-config__<PAGE_KEY>' path will be mapped to a path configured under <PAGE_KEY>
-  readonly configurablePathPrefix = '__cx-config__';
+  readonly configPathPrefix = '__cx-config__';
 
   ngOnInit() {
     console.log(this.config.routePaths); // spike todo remove
     console.log(this.router.config); // spike todo remove
 
-    const configuredRoutes = this.configurePaths(
-      this.config.routePaths,
-      this.router.config
-    );
+    const configuredRoutes = this.configureRoutes(this.router.config);
     this.router.resetConfig(configuredRoutes);
     console.log(this.router.config); // spike todo remove
   }
 
   // TODO: take care also of nested routes and matching them with __cx-config__ prefix
-  configurePaths(
-    routePathsConfig: AuthModuleConfig['routePaths'],
-    existingRoutes: Routes
-  ): Routes {
-    return existingRoutes.map(route => {
-      this.configurePath(route, 'path', routePathsConfig);
-      this.configurePath(route, 'redirectTo', routePathsConfig);
-      return route;
+  configureRoutes(existingRoutes: Routes): Routes {
+    const configuredRoutes = [];
+    existingRoutes.forEach(route => {
+      // we assume that 'path' and 'redirectTo' cannot be both configurable for one route
+      if (this.isConfigurable(route.path)) {
+        configuredRoutes.push(...this.configurePath(route));
+      } else if (this.isConfigurable(route.redirectTo)) {
+        configuredRoutes.push(this.configureRedirect(route));
+      } else {
+        configuredRoutes.push(route);
+      }
     });
+    return configuredRoutes;
   }
 
-  configurePath(
-    route: Route,
-    key: 'path' | 'redirectTo',
-    routePathsConfig: AuthModuleConfig['routePaths']
-  ) {
-    const path = route[key];
-    if (
-      typeof path === 'string' &&
-      path.startsWith(this.configurablePathPrefix)
-    ) {
-      const configurablePathKey = path.replace(this.configurablePathPrefix, '');
-      const configuredPath = routePathsConfig[configurablePathKey];
+  private isConfigurable(path: string): boolean {
+    return typeof path === 'string' && path.startsWith(this.configPathPrefix);
+  }
 
-      if (configuredPath === undefined) {
-        if (!this.config.production) {
-          console.warn(`No path was configured for '${path}'!`);
-        }
-      } else {
-        route[key] = configuredPath;
-      }
+  private configurePath(route: Route): Route[] {
+    return this.getConfiguredPaths(route.path).map(configuredPath =>
+      Object.assign({}, route, { path: configuredPath })
+    );
+  }
+
+  private configureRedirect(route: Route): Route {
+    const redirectCandidates = this.getConfiguredPaths(route.redirectTo);
+    const newRedirect = redirectCandidates[0]; // get the first candidate by convention
+    return Object.assign({}, route, { redirectTo: newRedirect });
+  }
+
+  private getConfiguredPaths(originalPath: string): string[] {
+    const configKey = originalPath.replace(this.configPathPrefix, '');
+    const configuredPaths = this.config.routePaths[configKey];
+    if (configuredPaths === undefined && !this.config.production) {
+      console.warn(`No paths were configured for '${originalPath}'!`);
     }
-    return route;
+    return configuredPaths;
   }
 }
