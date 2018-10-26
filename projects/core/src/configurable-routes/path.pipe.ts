f55bf3bd9fdf09964fa433de1e41a0ae5732156d
Krzysztof Platis <platonn.git@gmail.com>
2018-10-26 16:08:01 +0200

    feature/SPA-1403-demo: implement strategy for cxPath - find first path that matches all its parameters to parameters object

--- projects/core/src/configurable-routes/path.pipe.ts
+++ projects/core/src/configurable-routes/path.pipe.ts
@@ -4,11 +4,6 @@ import { ServerConfig } from '../config/server-config/server-config';
 type Segment = string;
 type Path = string;
 type Parameter = string;
-interface PathWithMatchingParameters {
-  path: Path;
-  matchingParameters: Parameter[];
-  isMatchingAllParameters: boolean;
-}
 
 @Pipe({
   name: 'cxPath'
@@ -19,42 +14,39 @@ export class PathPipe implements PipeTransform {
 
   // always returns an absolute path (with leading /)
   transform([pageName, parametersObject]: [string, object]): Segment[] {
-    const pathCandidates = this.config.routePaths[pageName];
+    const paths = this.config.routePaths[pageName];
 
-    if (pathCandidates === undefined) {
+    if (paths === undefined) {
       if (!this.config.production) {
         // TODO check if it really calls console.warn when pageName does not exist as a key in the config
         console.warn(`No paths were configured for page '${pageName}'!`);
       }
-      return ['']; // main route
+      return ['/']; // main route
     }
 
-    const {
-      path,
-      isMatchingAllParameters
-    } = this.getPathWithMostMatchingParameters(
-      pathCandidates,
+    const path = this.getFirstPathMatchingAllParameters(
+      paths,
       Object.keys(parametersObject)
     );
 
-    if (!this.config.production) {
-      if (!isMatchingAllParameters) {
-        // TODO: console.warn when no path matching fully its parameters with expected list
+    if (path === undefined) {
+      if (!this.config.production) {
         // TODO check if it does print nicely
         console.warn(
-          `Selected path did not match all its parameters with given parameters. Path: `,
+          `No configured path matches all its parameters to given parameters object. Configured paths: `,
           path,
-          `. Given parameters: `,
-          Object.keys(parametersObject)
+          `. Parameters object: `,
+          parametersObject
         );
       }
+      return ['/']; // main route
     }
 
-    const absolutePath = this.ensureLeadingSlash(path);
-    return this.injectParametersValues(absolutePath, parametersObject);
+    const absolutePath = this.ensureLeadingSlash(path); // TODO: rethink if always return absulte path - for configurable child routes
+    return this.injectParameterValues(absolutePath, parametersObject);
   }
 
-  private injectParametersValues(path: Path, parametersObject: object) {
+  private injectParameterValues(path: Path, parametersObject: object) {
     return this.getSegments(path).map(
       segment =>
         this.isParameter(segment)
@@ -63,58 +55,15 @@ export class PathPipe implements PipeTransform {
     );
   }
 
-  private ensureLeadingSlash(path: Path): Path {
-    return (path = path.startsWith('/') ? path : '/' + path);
-  }
-
-  // returns first path with most matching parameters and list of matching parameters
-  // TODO think of a way for Client to define his own function to pick best path (for each module separate)
-
-  private getPathWithMostMatchingParameters(
-    pathCandidates: Path[],
+  private getFirstPathMatchingAllParameters(
+    paths: Path[],
     givenParameters: Parameter[]
-  ): PathWithMatchingParameters {
-    const pathsWithMatchingParameters = pathCandidates.map(path =>
-      this.getPathWithMatchingParameters(path, givenParameters)
-    );
-
-    if (
-      !this.config.production &&
-      !pathsWithMatchingParameters.some(
-        candidate => candidate.isMatchingAllParameters
+  ): Path {
+    return paths.find(path =>
+      this.getParameters(path).every(
+        parameter => givenParameters.indexOf(parameter) !== -1
       )
-    ) {
-      console.warn(
-        `No configured path matching its parameters with all given parameters. Configured paths: `,
-        pathCandidates,
-        `. Given parameters: `,
-        givenParameters
-      );
-    }
-
-    return pathsWithMatchingParameters.reduce(
-      (best, current) =>
-        current.matchingParameters.length > best.matchingParameters.length
-          ? current
-          : best,
-      { path: '', matchingParameters: [], isMatchingAllParameters: false }
     );
-  }
-
-  private getPathWithMatchingParameters(
-    path: Path,
-    givenParameters: Parameter[]
-  ): PathWithMatchingParameters {
-    const pathParameters = this.getParameters(path);
-    const matchingParameters = pathParameters.filter(
-      parameter => givenParameters.indexOf(parameter) !== -1
-    );
-    return {
-      path,
-      matchingParameters,
-      isMatchingAllParameters:
-        matchingParameters.length === pathParameters.length
-    };
   }
 
   private getParameters(path: Path) {
@@ -133,5 +82,9 @@ export class PathPipe implements PipeTransform {
 
   private getParameterName(segment: Segment): Parameter {
     return segment.slice(1); // remove leading ':'
+  }
+
+  private ensureLeadingSlash(path: Path): Path {
+    return (path = path.startsWith('/') ? path : '/' + path);
   }
 }
