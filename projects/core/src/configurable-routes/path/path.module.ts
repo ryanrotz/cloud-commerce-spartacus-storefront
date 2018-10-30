6e7d04c70b233a7aca1bd5b9ede477880af9a0c3
Krzysztof Platis <platonn.git@gmail.com>
2018-10-30 13:16:06 +0100

    feature/SPA-1403: remove smart-path.pipe POC

--- projects/core/src/configurable-routes/path/path.module.ts
+++ projects/core/src/configurable-routes/path/path.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PathPipe } from './path.pipe';
-import { SmartPathPipe } from './smart-path.pipe';
 import { PathService } from './path.service';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PathPipe, SmartPathPipe],
-  exports: [PathPipe, SmartPathPipe],
+  declarations: [PathPipe],
+  exports: [PathPipe],
   providers: [PathService] // spike todo: consider if not to many instances of PathService will be created
 })
 export class PathModule {}
