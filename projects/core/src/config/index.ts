c33a2a99a60a52ed52d2645dfe5c04c5dcd91db9
Krzysztof Platis <platonn.git@gmail.com>
2018-10-30 11:38:38 +0100

    feature/SPA-1403: expose configurable-routes directly via public_api of core

--- projects/core/src/config/index.ts
+++ projects/core/src/config/index.ts
@@ -2,7 +2,3 @@ export * from './config.module';
 export * from './server-config/server-config';
 export * from './server-config/server-config-validator';
 export * from './utils/config-validator';
-
-// TODO spike fix paths for exports not to start with ../
-export * from '../configurable-routes/path/path.module';
-export * from '../configurable-routes/configurable-routes.module';
---
+++ projects/core/src/configurable-routes/index.ts
@@ -0,0 +1,2 @@
+export * from './path/path.module';
+export * from './configurable-routes.module';
