dce1616368f3d74767d9ad27633c42cbc47080f5
Krzysztof Platis <platonn.git@gmail.com>
2018-11-02 09:23:38 +0100

    feature/SPA-1403: fix problem with tertiary-bar.component that called pathService too early

--- projects/storefrontlib/src/lib/ui/layout/header/tertiary-bar/tertiary-bar.component.ts
+++ projects/storefrontlib/src/lib/ui/layout/header/tertiary-bar/tertiary-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PathService } from '@spartacus/core';
 
 @Component({
@@ -6,21 +6,25 @@ import { PathService } from '@spartacus/core';
   templateUrl: './tertiary-bar.component.html',
   styleUrls: ['./tertiary-bar.component.scss']
 })
-export class TertiaryBarComponent {
+export class TertiaryBarComponent implements OnInit {
   constructor(private pathService: PathService) {}
 
-  tertiaryNavItems: any[] = [
-    {
-      label: 'Sale',
-      url: this.pathService.transform('sale')
-    },
-    {
-      label: 'Contact us',
-      url: this.pathService.transform('contact')
-    },
-    {
-      label: 'Help',
-      url: this.pathService.transform('help')
-    }
-  ];
+  tertiaryNavItems: { url: string; label: string }[];
+
+  ngOnInit(): void {
+    this.tertiaryNavItems = [
+      {
+        label: 'Sale',
+        url: this.pathService.transform('sale')
+      },
+      {
+        label: 'Contact us',
+        url: this.pathService.transform('contact')
+      },
+      {
+        label: 'Help',
+        url: this.pathService.transform('help')
+      }
+    ];
+  }
 }
