f4402e8c91de6703d2e800f7c7e44b4e172bd5de
Krzysztof Platis <platonn.git@gmail.com>
2018-11-05 14:04:41 +0100

    feature/SPA-1403: use cxDynamicPathPipe in components: footer-navigation, link and navigation-ui

--- projects/storefrontlib/src/lib/cms-lib/footer-navigation/footer-navigation.module.ts
+++ projects/storefrontlib/src/lib/cms-lib/footer-navigation/footer-navigation.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FooterNavigationComponent } from './footer-navigation.component';
+import { PathModule } from '@spartacus/core';
+import { GenericLinkModule } from '../../ui/components/generic-link/generic-link.module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, GenericLinkModule, PathModule],
   declarations: [FooterNavigationComponent],
   entryComponents: [FooterNavigationComponent],
   exports: [FooterNavigationComponent]
