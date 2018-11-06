06ad0b0d2315eaf8c10bbe8fc1e93343016f6b81
Krzysztof Platis <platonn.git@gmail.com>
2018-11-06 11:48:40 +0100

    feature/SPA-1403-copying: make routesConfig options field of configurable routes module config

--- projects/core/src/configurable-routes/configurable-routes-module.config.ts
+++ projects/core/src/configurable-routes/configurable-routes-module.config.ts
@@ -2,7 +2,7 @@ import { RoutesConfig, defaultRoutesConfig } from './routes-config';
 
 // SPIKE TODO: rename it better:
 export abstract class ConfigurableRoutesModuleConfig {
-  routesConfig: RoutesConfig;
+  routesConfig?: RoutesConfig;
 }
 
 export const defaultConfigurableRoutesModuleConfig = {
